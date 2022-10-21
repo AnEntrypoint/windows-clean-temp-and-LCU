623ab540846d6b9d1eabea2f70bc11f41e212523
lanmower <almagestfraternite@gmail.com>
2022-10-21 16:31:45 +0200

    Add files via upload

---
+++ clean.bat
@@ -0,0 +1,39 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------    
+
+DEL /F/Q/S c:\windows\servicing\lcu\* 
+RMDIR /Q/S c:\windows\servicing\lcu\*
+DEL /F/Q/S c:\Windows\Temp\* 
+RMDIR /Q/S c:\Windows\Temp\*
+DEL /F/Q/S %userprofile%\AppData\Local\Temp\* 
+RMDIR /Q/S %userprofile%\AppData\Local\Temp\*
+Dism.exe /online /Cleanup-Image /StartComponentCleanup
+start c:\windows\SYSTEM32\cleanmgr.exe
